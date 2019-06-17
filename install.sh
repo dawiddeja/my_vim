a58ca09801085ee0f96e69910862d9326917f952
Dawid Deja <dawid.deja@corp.ovh.com>
2019-06-17 11:09:43 +0200

    Add installation for YCM internals

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc ~/.vimrc
+
+sudo apt install build-essential cmake python3-dev
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py
+
 vim -e "+PluginInstall|:q!"
