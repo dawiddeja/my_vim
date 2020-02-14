26edade46bdcb45b428f836655bdcffb25108985
Dawid Deja <dawdej@gmail.com>
2020-02-14 16:39:36 +0100

    Fix installation issue with vim

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc ~/.vimrc
 
 sudo apt install build-essential cmake python3-dev
+
+vim -e +PluginInstall +qall
+
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py
-
-vim -e "+PluginInstall|:q!"
