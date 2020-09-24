701fd52b869e07121314fe5b9c1d01cc94407f65
Dawid Deja <dawid.deja@corp.ovh.com>
2020-09-24 09:58:10 +0200

    Fix for making it working in 18.04

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ sudo apt install build-essential cmake python3-dev
 vim -e +PluginInstall +qall
 
 cd ~/.vim/bundle/YouCompleteMe
+# Fix for making YouCompleteMe on ubuntu 18.04
+git checkout d98f896ada495c3687007313374b2f945a2f2fb4
 python3 install.py
