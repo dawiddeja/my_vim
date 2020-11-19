4064f8ea71a636db7e387fee23d01456629b215d
Dawid Deja <dawid.deja@corp.ovh.com>
2020-11-19 17:00:23 +0100

    Fix for not compiling ycmd under ubuntu 18.04

--- install.sh
+++ install.sh
@@ -10,4 +10,10 @@ vim -e +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 # Fix for making YouCompleteMe on ubuntu 18.04
 git checkout d98f896ada495c3687007313374b2f945a2f2fb4
+# Fix for making ycmd compile on Ubuntu 18.04
+cd third_party/ycmd
+git checkout 0c543a9c928032343ba65884a29fd06c4b8d0aa2
+cd ../..
+git submodule update --init --recursive
+
 python3 install.py
